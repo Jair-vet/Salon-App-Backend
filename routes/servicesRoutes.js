95fa7e534b538e80e8b872df6afa16e5f5122e70
Jair <jairaceves56@gmail.com>
2023-12-14 13:33:07 -0600

    Install mongose

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -1,25 +1,12 @@
 import express from 'express'
+import { services } from '../data/beautyServices.js'
 
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
 
-    const products = [
-        {
-            id: 1,
-            price: 50,
-            name: 'Laptop'
-        },
-        {   
-            id: 2,
-            price: 89,
-            name: 'Camara'
-
-        }
-    ]
-
-    res.json(products)
+    res.json(services)
 })
 
 export default router
