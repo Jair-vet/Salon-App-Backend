8f3b3f63e93c9aae6428a7e8dc2fd6776013b11a
Jair <jairaceves56@gmail.com>
2023-12-21 09:46:04 -0600

    JSONwebToken added

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-// import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 // import { format } from 'date-fns'
 // import es from 'date-fns/locale/es/index.js'
 
@@ -18,16 +18,16 @@ function handleNotFoundError(message, res ) {
         msg: error.message
     })
 }
-// 
-// const uniqueId = () => Date.now().toString(32) + Math.random().toString(32).substring(2)
-// 
-// const generateJWT = (id) => {
-//     const token = jwt.sign( { id }, process.env.JWT_SECRET, {
-//         expiresIn: '30d'
-//     })
-//     return token
-// }
-// 
+
+const uniqueId = () => Date.now().toString(32) + Math.random().toString(32).substring(2)
+
+const generateJWT = (id) => {
+    const token = jwt.sign( { id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+    return token
+}
+
 // function formatDate(date) {
 //     return format(date, 'PPPP', {locale: es})
 // }
@@ -37,7 +37,7 @@ function handleNotFoundError(message, res ) {
 export {
     validateObjectId,
     handleNotFoundError,
-    // uniqueId,
-    // generateJWT,
+    uniqueId,
+    generateJWT,
     // formatDate
 }
